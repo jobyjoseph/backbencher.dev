fc556b70ce8b6c09ccc28a932d6d8b131196a856
Joby Joseph <reachjoby@gmail.com>
2021-06-29 18:20:19 +0530

    add Header and Layout components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,39 @@
 import React from "react";
-import { useSiteMetadata } from "../hooks/use-site-metadata";
+import { Link, graphql } from "gatsby";
 
-export default function IndexPage() {
-  const { title, description } = useSiteMetadata();
+const IndexPage = ({ data }) => {
+  console.log(data);
   return (
     <>
-      <h1>{title}</h1>
-      <p>{description}</p>
+      {data.allMdx.nodes.map(({ id, excerpt, frontmatter, slug }) => (
+        <div>
+          <Link to={`/${slug}`}>
+            <h1>{frontmatter.title}</h1>
+          </Link>
+          <div>
+            <small>{frontmatter.date}</small>
+          </div>
+          <p>{excerpt}</p>
+        </div>
+      ))}
     </>
   );
-}
+};
+
+export const query = graphql`
+  query SITE_INDEX_QUERY {
+    allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
+      nodes {
+        id
+        excerpt(pruneLength: 250)
+        frontmatter {
+          title
+          date(formatString: "YYYY MMMM Do")
+        }
+        slug
+      }
+    }
+  }
+`;
+
+export default IndexPage;
