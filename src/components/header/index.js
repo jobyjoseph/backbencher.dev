cbd05d5875c7d64fff13e30beae27a0a7a764c22
Joby Joseph <reachjoby@gmail.com>
2021-07-19 03:07:09 +0530

    Netlify

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,7 +6,7 @@ import * as styles from "./header.module.scss";
 const Header = (props) => (
   <header>
     <div className="container">
-      <Link to="/" className={styles.logoContainer}>
+      <Link to="/articles" className={styles.logoContainer}>
         <img src={logo} alt="Backbencher.dev" className={styles.logo} />
         <span className={styles.logoText}>Backbencher.dev</span>
       </Link>
