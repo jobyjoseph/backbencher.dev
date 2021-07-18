8d5a9ab96537dc29f73a0e39b358c0f1ca5db7f9
Joby Joseph <reachjoby@gmail.com>
2021-07-19 03:05:34 +0530

    Netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-netlify`,
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
