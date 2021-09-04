71f1ca9fac710394e0b4f2be94ab6796c5638fdb
Joby Joseph <reachjoby@gmail.com>
2021-09-04 15:34:40 +0530

    Added description

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -7,7 +7,6 @@
 
 import React from "react";
 import PropTypes from "prop-types";
-import Helmet from "react-helmet";
 import Header from "../header";
 import Footer from "../footer";
 import SEO from "../seo";
