de21a3cc30184339d8be1d2b2a53750f932cf514
Joby Joseph <reachjoby@gmail.com>
2022-08-24 12:36:38 -0700

    usecontext hook

--- src/components/olink/index.js
+++ src/components/olink/index.js
@@ -1,8 +1,21 @@
 import React from "react";
+import * as styles from "./olink.module.scss";
 
-export default function ({ link, children, bold }) {
+export default function ({
+  link,
+  children,
+  bold,
+  type = "link",
+  background = "link",
+}) {
   return (
-    <a href={link} style={{fontWeight: bold?700:400}} target="_blank" rel="nofollow noreferrer">
+    <a
+      href={link}
+      style={{ fontWeight: bold ? 700 : 400 }}
+      className={`${styles[type]} ${styles[background]}`}
+      target="_blank"
+      rel="nofollow noreferrer"
+    >
       {children}
     </a>
   );
