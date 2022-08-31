e22b1d94f7c5fd2f2fb38aa1981cfeb56f849f15
Joby Joseph <reachjoby@gmail.com>
2022-08-31 16:27:21 -0700

    Grouping index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,65 @@ import React from "react";
 import { Link, graphql } from "gatsby";
 import Layout from "../components/layout";
 import * as styles from "../styles/index.module.scss";
+import * as config from "../config/tags";
 
 const IndexPage = ({ data }) => {
+  const months = {
+    0: "JAN",
+    1: "FEB",
+    2: "MAR",
+    3: "APR",
+    4: "MAY",
+    5: "JUN",
+    6: "JUL",
+    7: "AUG",
+    8: "SEP",
+    9: "OCT",
+    10: "NOV",
+    11: "DEC",
+  };
+
+  const groups = [];
+  let groupSlNo = 0;
+
   return (
     <Layout title="React.js | JavaScript | Next.js | Web Articles">
-      <h1 className={styles.mainHeader}>All Articles</h1>
-      {data.allMdx.nodes.map(({ frontmatter }, index) => (
-        <div className={styles.item} key={index}>
-          <Link to={`/articles/${frontmatter.slug}`}>
-            {`${frontmatter.title}`}
-          </Link>
-        </div>
-      ))}
+      {data.allMdx.nodes.map(({ frontmatter }, index) => {
+        let showDateHeader = false;
+        const tag = frontmatter.tags;
+        const date = new Date(frontmatter?.date);
+        const dateHeader = `${months[date.getMonth()]} ${date.getFullYear()}`;
+        if (!groups.includes(dateHeader)) {
+          groups.push(dateHeader);
+          showDateHeader = true;
+          groupSlNo = 1;
+        } else {
+          groupSlNo++;
+        }
+        return (
+          <>
+            {showDateHeader && (
+              <h6 className={styles.mainHeader}>{dateHeader}</h6>
+            )}
+            <div className={styles.item} key={index}>
+              <span className={styles.slno}>{groupSlNo}.</span>
+              <div>
+                <Link to={`/articles/${frontmatter.slug}`}>
+                  {`${frontmatter.title}`}
+                </Link>
+                <span className={styles.tagContainer}>
+                  <span
+                    className={styles.tag}
+                    style={{ color: config[tag]?.color }}
+                  >
+                    {config[tag]?.title}
+                  </span>
+                </span>
+              </div>
+            </div>
+          </>
+        );
+      })}
     </Layout>
   );
 };
@@ -28,6 +75,7 @@ export const query = graphql`
           title
           date(formatString: "D MMM, YY")
           slug
+          tags
         }
       }
     }
